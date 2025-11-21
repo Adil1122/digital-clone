86daf2b3efcf2dc6c8472df5dea6ec21b4c955f7
Your Name <ahsan@timeahead.co>
2025-11-21 21:07:22 +0500

    Enable page vertical scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     >
       <head />
       <body className="min-h-screen bg-black text-white">
-        <main className="relative flex flex-col gap-6 h-screen w-screen overflow-hidden">
+        <main className="relative flex flex-col gap-6 h-screen w-screen overflow-x-hidden">
           <NavBar />
           {children}
         </main>
